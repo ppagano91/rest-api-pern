9c291e3fc0f0c9609db2d01d50bf1a4c98f10368
Patricio Pagano <pagano.patricio@gmail.com>
2024-06-18 08:50:30 -0300

    Modelo Producto

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,11 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const db = new Sequelize(process.env.DATABASE_URL,
     {
+        models: [__dirname + '/../models/**/*.ts'],
         dialectOptions: {
             ssl: {
                 require: false
