f6b010c5a3d128c0ef1b8a649d3a77b386e60683
Patricio Pagano <pagano.patricio@gmail.com>
2024-06-20 17:12:05 -0300

    Middleware para errores de validación

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -4,25 +4,17 @@ import Product from "../models/Product.model"
 
 export const createProduct = async (req: Request, res: Response) => {
 
-    await check("name")
-            .notEmpty().withMessage("El nombre del Producto no puede estar vacío").run(req);
-    await check("price")
-            .isNumeric().withMessage("El valor del precio debe ser un número")
-            .notEmpty().withMessage("El precio del Producto no puede estar vacío")
-            .custom((value)=>{return value > 0}).withMessage("El precio del Producto debe ser mayor a 0")
-            .run(req)
-
-    let errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json({error:errors.array()})
+    try {
+        /* OPCION 1
+        const product = new Product(req.body);
+        const savedProduct = await product.save();
+        */
+       /* OPCION 2*/
+       const product = await Product.create(req.body)
+       res.json({
+           data: product
+        });
+    } catch (error) {
+        console.log(error)
     }
-    /* OPCION 1
-    const product = new Product(req.body);
-    const savedProduct = await product.save();
-    */
-   /* OPCION 2*/
-   const product = await Product.create(req.body)
-    res.json({
-        data: product
-    });
 }
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { createProduct } from "./handlers/product";
+import { body } from "express-validator"
+import { handleInputErrors } from "./middleware";
 
 const router = Router();
 
@@ -7,7 +9,15 @@ router.get("/", (req, res) =>{
     res.json("GET")
 })
 
-router.post("/", createProduct)
+router.post("/",
+    body("name")
+            .notEmpty().withMessage("El nombre del Producto no puede estar vacío"),
+    body("price")
+            .isNumeric().withMessage("El valor del precio debe ser un número")
+            .notEmpty().withMessage("El precio del Producto no puede estar vacío")
+            .custom((value)=>{return value > 0}).withMessage("El precio del Producto debe ser mayor a 0"),
+    handleInputErrors,
+    createProduct)
 
 router.put("/", (req, res) =>{
     res.json("PUT")
